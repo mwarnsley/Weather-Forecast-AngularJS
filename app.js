a4d3a1a05e209781806d4c6176279047b05234b9
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-05-12 20:38:20 -0400

    Adding the pages needed along with the controller structure for the pages

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-//Module
+//MODULE
 var weatherApp = angular.module("weatherApp", ['ngRoute', 'ngResource']);
 
+//CONTROLLERS
+weatherApp.controller("homeController", ["$scope", function($scope){
+    
+}]);
+
+weatherApp.controller("forecastController", ["$scope", function($scope){
+    
+}]);
