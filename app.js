5842f670ea4196bc9a709bd6e8aff86356741e4f
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-05-12 20:46:53 -0400

    Editing the forecast and home page for demo text along with adding in the app js page on the first page to show the routing that was initial setup in the app js file

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 //MODULE
 var weatherApp = angular.module("weatherApp", ['ngRoute', 'ngResource']);
 
+//ROUTES
+weatherApp.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: "pages/home.html",
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+            templateUrl: "pages/forecast.html",
+            controller: "forecastController"
+        })
+});
+
 //CONTROLLERS
 weatherApp.controller("homeController", ["$scope", function($scope){
     
